58512de7b55cb0374e63c6eb00585f3fe4abaafd
Justin Fulton <jusfulton@gmail.com>
2021-06-29 20:48:21 -0700

    breaking due to v 2

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./about-css-modules.module.css"
+import Container from "../components/container"
+console.log(styles)
+export default function About() {
+  return (
+    <Container>
+      <h1>About CSS Modules</h1>
+      <p>CSS Modules are cool</p>
+    </Container>
+  )
+}
